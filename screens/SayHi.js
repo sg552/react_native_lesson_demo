560485ea436198c2e8e0a768d56a1527af3c9a76
siwei <shensiwei@sina.com>
2018-12-25 09:49:21 +0800

    修复了bug:  say_hi 页面报错

--- screens/SayHi.js
+++ screens/SayHi.js
@@ -4,6 +4,7 @@ import {Platform, StyleSheet, Text, View, Button} from 'react-native'
 export default class SayHi extends React.Component{
 
   render(){
+    const name = this.props.navigation.getParam('name', '')
     return (
       <View>
         <Text>
